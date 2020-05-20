23748ecb2fd8f9b3510120335c7aec9cf7bcdc20
llhuang <15221266039@139.com>
2020-05-20 19:34:34 +0800

    [MINOR] [SCRIPT] [UPDATE] 1. runListUpdate.sh    a. created
    [MINOR] [ROOT] [UPDATE]
    1. .gitignore
       a. created (empty now)

---
+++ script/runListUpdate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+  #
+  #  Filename       : runListUpdate.sh
+  #  Author         : Huang Leilei
+  #  Created        : 2020-05-20
+  #  Description    : a script-based update list
+  #
+#-------------------------------------------------------------------------------
+
+#*** SUB FUNCTION **************************************************************
+
+
+#*** MAIN BODY *****************************************************************
+  # removing empty directories
+  cd ../
+    echo "removing empty directories..."
+    dirEmpty=$(find . -type d -empty)
+    while [ "$dirEmpty" != "" ]
+    do
+      for dirCur in $dirEmpty
+      do
+        echo "  $dirCur"
+      done
+      echo "  ---"
+      rm -rf $dirEmpty
+      dirEmpty=$(find .. -type d -empty)
+    done
+  cd - > /dev/null
+  echo ""
+
+  # log time
+  date
+  echo ""
