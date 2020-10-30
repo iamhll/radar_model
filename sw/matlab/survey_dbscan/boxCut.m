acbdd638ac529401ac04893bda75507ef9e67779
llhuang <15221266039@139.com>
2020-10-30 10:04:18 +0800

    - [MINOR] [SW/M] [UPDATE] 1. survey_dbscan a. sort of datPnt added b. update of dat*I added

--- sw/matlab/survey_dbscan/boxCut.m
+++ sw/matlab/survey_dbscan/boxCut.m
@@ -17,34 +17,44 @@ function [idxGrp] = boxCut(datPnt)
 
 
 %*** MAIN BODY *****************************************************************
-    [num, siz] = size(datPnt);
+    [num, ~] = size(datPnt);
+    [~, idxSort] = sort(datPnt(:,IDX_RNG),'descend');
     idxGrp = zeros(num, 1);
     idxGrpCur = 0;
-    for i = 1:num
+    for iFor = 1:num
+        i = idxSort(iFor);
         if idxGrp(i) == 0
             idxGrpCur = idxGrpCur + 1;
             idxGrp(i) = idxGrpCur;
-            datRngI = datPnt(i,IDX_RNG);
-            datVelI = datPnt(i,IDX_VEL);
-            datAngI = datPnt(i,IDX_ANG);
+            datRngI  = datPnt(i,IDX_RNG);
+            datVelI  = datPnt(i,IDX_VEL);
+            datAngI  = datPnt(i,IDX_ANG);
             datRngIX = datRngI * cos(datAngI);
             datVelIX = datVelI * sin(datAngI);
             datRngIY = datRngI * cos(datAngI);
             datVelIY = datVelI * sin(datAngI);
-            for j = i+1:num
-                datRngJ = datPnt(j,IDX_RNG);
-                datVelJ = datPnt(j,IDX_VEL);
-                datAngJ = datPnt(j,IDX_ANG);
+            for jFor = iFor+1:num
+                j = idxSort(jFor);
+                datRngJ  = datPnt(j,IDX_RNG);
+                datVelJ  = datPnt(j,IDX_VEL);
+                datAngJ  = datPnt(j,IDX_ANG);
                 datRngJX = datRngJ * cos(datAngI);
                 datVelJX = datVelJ * sin(datAngI);
                 datRngJY = datRngJ * cos(datAngI);
                 datVelJY = datVelJ * sin(datAngI);
-                if   abs(datRngIY - datRngJY) < 5                ...
-                  && abs(datRngIX - datRngJX) < 2                ...
-                  && abs(datVelIY - datVelJY) < 1.5              ...
-                  && abs(datVelIX - datVelJX) < 1                ...
-                  && abs(datAngI  - datAngJ ) < 20 / 180 * pi
+                if   abs(datRngIY - datRngJY) <= 5                ...
+                  && abs(datRngIX - datRngJX) <= 2                ...
+                  && abs(datVelIY - datVelJY) <= 1.5              ...
+                  && abs(datVelIX - datVelJX) <= 1                ...
+                  && abs(datAngI  - datAngJ ) <= 20 / 180 * pi
                     idxGrp(j) = idxGrpCur;
+                    datRngI   = datRngJ ;
+                    datVelI   = datVelJ ;
+                    datAngI   = datAngJ ;
+                    datRngIX  = datRngJX;
+                    datVelIX  = datVelJX;
+                    datRngIY  = datRngJY;
+                    datVelIY  = datVelJY;
                 end
             end
         end
